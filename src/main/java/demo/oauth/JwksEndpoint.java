781407f1d5156fc210c54e6134ef56357222f6da
Ingo Strauch <ingo.strauch@otto.de>
2024-04-29 14:10:56 +0200

    Log when JWKS is requested and print KID

--- src/main/java/demo/oauth/JwksEndpoint.java
+++ src/main/java/demo/oauth/JwksEndpoint.java
@@ -3,17 +3,20 @@ package demo.oauth;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 class JwksEndpoint {
 
     private final RSAKey jweEncKey;
 
     @GetMapping("/oauth/jwks")
     Object getJwks() {
+        log.info("Returning jwks: " + jweEncKey.getKeyID());
         return new JWKSet(jweEncKey).toJSONObject();
     }
 }

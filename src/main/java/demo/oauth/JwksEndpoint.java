918ba2813e822a993c88e5f854adb5060ac3d6fb
Ingo Strauch <ingo.strauch@otto.de>
2024-04-29 18:08:33 +0200

    No need to modify key here

--- src/main/java/demo/oauth/JwksEndpoint.java
+++ src/main/java/demo/oauth/JwksEndpoint.java
@@ -1,10 +1,7 @@
 package demo.oauth;
 
-import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,15 +15,9 @@ class JwksEndpoint {
     private final RSAKey jweEncKey;
 
     @GetMapping("/oauth/jwks")
-    Object getJwks() throws JOSEException {
+    Object getJwks() {
         log.info("Returning jwks: " + jweEncKey.getKeyID());
 
-        RSAKey jwk = new RSAKey.Builder((RSAPublicKey) jweEncKey.toPublicKey())
-            .privateKey((RSAPrivateKey) jweEncKey.toPrivateKey())
-            .keyID(jweEncKey.getKeyID())
-            .keyUse(com.nimbusds.jose.jwk.KeyUse.SIGNATURE) // keys from keytool have 'use: "enc"'
-            .build();
-
-        return new JWKSet(jwk).toJSONObject();
+        return new JWKSet(jweEncKey).toJSONObject();
     }
 }

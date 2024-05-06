3a8eeafbe297d9240b3356adbc4d4dc6b589f48d
Ingo Strauch <ingo.strauch@otto.de>
2024-05-06 14:07:01 +0200

    Fix security config

--- src/main/java/demo/config/WebSecurityConfig.java
+++ src/main/java/demo/config/WebSecurityConfig.java
@@ -6,26 +6,27 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final KeycloakJwtAuthenticationConverter jwtAuthenticationConverter;
+  private final KeycloakJwtAuthenticationConverter jwtAuthenticationConverter;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
 
-        http.csrf(c -> {
-            c.ignoringRequestMatchers(new AntPathRequestMatcher("/oauth/jwks"), new AntPathRequestMatcher("/api/**"));
-        }).authorizeRequests(c -> {
-            c.mvcMatchers("/api/claims").hasAuthority("SCOPE_openid")
-                    .mvcMatchers("/oauth/jwks").anonymous()
-                    .antMatchers(HttpMethod.POST, "/oauth/decrypt").anonymous()
-                    .anyRequest().anonymous();
+    http
+        .csrf(AbstractHttpConfigurer::disable)
+        .authorizeRequests(c -> {
+          c
+              .mvcMatchers("/api/claims").hasAuthority("SCOPE_openid")
+              .antMatchers("/oauth/**").anonymous()
+              .antMatchers(HttpMethod.POST, "/oauth/decrypt").anonymous()
+              .anyRequest().authenticated();
         }).oauth2ResourceServer().jwt(c -> {
-            c.jwtAuthenticationConverter(jwtAuthenticationConverter);
+          c.jwtAuthenticationConverter(jwtAuthenticationConverter);
         });
-    }
+  }
 }
